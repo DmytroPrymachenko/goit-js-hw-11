b0199a9398018f3c8fd857bafacd1f032e5455bb
DmytroPrymachenko <144675672+DmytroPrymachenko@users.noreply.github.com>
2023-11-17 18:10:06 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -1,34 +1,196 @@
-// import '../src/settengAPI';
-import { photoAPI } from './settengAPI';
+import { PhotoAPI } from './settengAPI';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const refs = {
-  form: document.querySelector(`.search-form`),
+  form: document.querySelector(`#search-form`),
   divElem: document.querySelector(`.gallery`),
-  btnSearch: document.querySelector(`.button-Search`),
+  btnSearch: document.querySelector(`.load-more`),
 };
+refs.btnSearch.disabled = true;
+refs.form.addEventListener('submit', onformElemSubmit);
+refs.btnSearch.addEventListener(`click`, onbtnSearchClick);
 
-refs.form.addEventListener('submit', onformElem);
-refs.btnSearch.addEventListener(`click`, onbtnSearchElem);
+const photosAPI = new PhotoAPI();
 
-function onformElem(e) {
+async function onformElemSubmit(e) {
   e.preventDefault();
+
   const formElem = e.target.elements.searchQuery.value;
-  console.log(formElem);
+
+  photosAPI.q = formElem;
+
+  photosAPI.page = 1;
+
+  try {
+    const res = await photosAPI.fetchPhoto();
+    photosAPI.totalPage = Math.ceil(res.totalHits / PhotoAPI.PER_PAGE);
+
+    refs.divElem.innerHTML = '';
+
+    renderTemplate(res.hits);
+
+    lightbox = new SimpleLightbox('.gallery a');
+
+    refs.btnSearch.disabled = false;
+
+    updateStatusLoadMore();
+  } catch (error) {
+    console.error('Error fetching photos:', error);
+  }
 }
 
-function photoTemplate() {
-  return `<div class="image-container">
-    <img 
-      src="${hits.webformatURL}"
-      alt="#"
-      class="image"
-    />`;
+function photoTemplate(photo) {
+  const {
+    webformatURL,
+    largeImageURL,
+    tags,
+    likes,
+    views,
+    comments,
+    downloads,
+  } = photo;
+
+  return `<a href="${largeImageURL}"><div class="photo-link">
+            <img class="photo" src="${webformatURL}" alt="${tags}" loading="lazy" />
+            <div class="photo-info">
+              <p class="photo-p">
+                <b>Likes</b>
+                <span>${likes}</span>
+              </p>
+              <p class="photo-p">
+                <b>Views</b>
+                <span>${views}</span>
+              </p>
+              <p class="photo-p">
+                <b>Comments</b>
+                <span>${comments}</span>
+              </p>
+              <p class="photo-p">
+                <b>Downloads</b>
+                <span>${downloads}</span>
+              </p>
+            </div>
+          </div></a>`;
 }
 
 function photosTemplate(photo) {
-  photo.map(photoTemplate).joim('');
+  const template = photo.map(photoTemplate).join('');
+  return template;
 }
+
 function renderTemplate(photo) {
-  const photoMarkap = photosTemplate(photo);
-  refs.divElem.insertAdjacentHTML('afterbegin', photoMarkap);
+  const photoMarkup = photosTemplate(photo);
+
+  refs.divElem.insertAdjacentHTML('beforeend', photoMarkup);
 }
+
+async function onbtnSearchClick(photo) {
+  photosAPI.page += 1;
+  try {
+    const res = await photosAPI.fetchPhoto();
+    renderTemplate(res.hits);
+    updateStatusLoadMore();
+  } catch (error) {
+    console.error('Error fetching more photos:', error);
+  }
+}
+
+function updateStatusLoadMore() {
+  refs.btnSearch.disabled = photosAPI.page >= photosAPI.totalPage;
+  console.log(photosAPI.page);
+  console.log(photosAPI.totalPage);
+}
+
+// // import '../src/settengAPI';
+// import { PhotoAPI } from './settengAPI';
+// import SimpleLightbox from 'simplelightbox';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// const refs = {
+//   form: document.querySelector(`#search-form`),
+//   divElem: document.querySelector(`.gallery`),
+//   btnSearch: document.querySelector(`.load-more`),
+// };
+// refs.btnSearch.disabled = true;
+// refs.form.addEventListener('submit', onformElemSubmit);
+// refs.btnSearch.addEventListener(`click`, onbtnSearchClick);
+
+// const photosAPI = new PhotoAPI();
+
+// async function onformElemSubmit(e) {
+//   e.preventDefault();
+
+//   const formElem = e.target.elements.searchQuery.value;
+
+//   photosAPI.q = formElem;
+
+//   photosAPI.page = 1;
+
+//   photosAPI.fetchPhoto().then(res => {
+//     photosAPI.totalPage = Math.ceil(res.totalHits / photosAPI.PER_PAGE);
+
+//     refs.divElem.innerHTML = '';
+
+//     renderTemplate(res.hits);
+
+//     lightbox = new SimpleLightbox('.gallery a');
+
+//     refs.btnSearch.disabled = false;
+
+//     updateStatusLoadMore();
+//   });
+// }
+
+// function photoTemplate(photo) {
+//   const {
+//     webformatURL,
+//     largeImageURL,
+//     tags,
+//     likes,
+//     views,
+//     comments,
+//     downloads,
+//   } = photo;
+
+//   return `<a href="${largeImageURL}"><div class="photo-card">
+//             <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+//             <div class="info">
+//               <p class="info-item">
+//                 <b>Likes</b>
+//                 <span>${likes}</span>
+//               </p>
+//               <p class="info-item">
+//                 <b>Views</b>
+//                 <span>${views}</span>
+//               </p>
+//               <p class="info-item">
+//                 <b>Comments</b>
+//                 <span>${comments}</span>
+//               </p>
+//               <p class="info-item">
+//                 <b>Downloads</b>
+//                 <span>${downloads}</span>
+//               </p>
+//             </div>
+//           </div></a>`;
+// }
+
+// function photosTemplate(photo) {
+//   const template = photo.map(photoTemplate).join('');
+//   return template;
+// }
+// function renderTemplate(photo) {
+//   const photoMarkup = photosTemplate(photo);
+//   // refs.divElem.innerHTML = photoMarkup;
+//   refs.divElem.insertAdjacentHTML('beforeend', photoMarkup);
+// }
+// function onbtnSearchClick(photo) {
+//   photosAPI.page += 1;
+//   photosAPI.fetchPhoto().then(res => renderTemplate(res.hits));
+//   updateStatusLoadMore();
+// }
+
+// function updateStatusLoadMore() {
+//   refs.btnSearch.disabled = photosAPI.page >= PhotoAPI.totalPage;
+// }
--- src/settengAPI.js
+++ src/settengAPI.js
@@ -1,19 +1,76 @@
 import Notiflix from 'notiflix';
 import axios from 'axios';
 
-const axiosPhoto = axios.create({
-  key: 
-  baseURL: `https://pixabay.com/api/?key=40708287-91fb1ed28a9cbdffb99391946&${key}&image_type=photo&orientation=horizontal&safesearch=true`,
-});
-
 export class PhotoAPI {
-  getPhotos() {
-    axiosPhoto
-      .get()
-      .then(res => res.data)
-      .catch(err => console.log(err));
+  static BASE_URL = 'https://pixabay.com';
+  static API_KEY = '40708287-91fb1ed28a9cbdffb99391946';
+  static END_POINT = '/api/';
+  static PER_PAGE = 40;
+
+  constructor() {
+    this.q = '';
+    this.page = 1;
+    this.totalPage = 1;
+  }
+
+  async fetchPhoto() {
+    const PARAMS = new URLSearchParams({
+      key: PhotoAPI.API_KEY,
+      q: this.q,
+      page: this.page,
+      per_page: PhotoAPI.PER_PAGE,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+    });
+
+    const url = `${PhotoAPI.BASE_URL}${PhotoAPI.END_POINT}?${PARAMS}`;
+
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        console.log(err);
+      }
+
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.log(err);
+      throw error;
+    }
   }
 }
-const photoAPI = new PhotoAPI();
 
-photoAPI.getPhotos();
+// import Notiflix from 'notiflix';
+// import axios from 'axios';
+
+// // const photoAPI = new PhotoAPI();
+// export class PhotoAPI {
+//   static BASE_URL = 'https://pixabay.com';
+//   static API_KEY = '40708287-91fb1ed28a9cbdffb99391946';
+//   static END_POINT = '/api/';
+//   static PER_PAGE = 40;
+//   constructor() {
+//     this.q = '';
+//     this.page = 1;
+//     this.totalPage = 1;
+//   }
+
+//   async fetchPhoto() {
+//     const PARAMS = new URLSearchParams({
+//       key: PhotoAPI.API_KEY,
+//       q: this.q,
+//       page: this.page,
+//       per_page: PhotoAPI.PER_PAGE,
+//       image_type: 'photo',
+//       orientation: 'horizontal',
+//       safesearch: true,
+//     });
+
+//     const url = `${PhotoAPI.BASE_URL}${PhotoAPI.END_POINT}?${PARAMS}`;
+//     const res =
+//     return fetch(url).then(res => res.json());
+//     // const res = await PhotoAPI.get(url);
+//     // return res.data;
+//   }
+// }
